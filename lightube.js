1cd45495ced07795253cdd841a214bab8c82f5f0
Raymond <raymond@lmws.net>
2011-08-24 19:44:35 -0700

    extract script out and use jsonp instead of XML

---
+++ lightube.js
@@ -0,0 +1,27 @@
+$('.videos').each(function(index, videos) {
+  var url = 'https://gdata.youtube.com/feeds/api/videos?author=' + $(videos).data('author') +'&max-results=50&alt=json-in-script&callback=?';
+  $.getJSON(url, function(data) {
+    $(data.feed.entry).each(function(index, item) {
+      var video_id = item.id.$t.match(/[\w]+$/i);
+      var img_url = item.media$group.media$thumbnail[2].url;
+      var title_text = item.title.$t;
+      var img = $('<img src="' + img_url + '" width="120" data-video-id="' + video_id + '" /> ');
+
+      img.click(function() {
+	$('#player').attr('src', "http://www.youtube.com/embed/" + $(this).data('video-id'));
+      });
+      img.attr('title', title_text);
+      img.css('cursor', 'pointer');
+
+      var div = $('<div></div>')
+      div.css('width', '120px');
+      div.css('height', '240px');
+      div.css('display', 'inline-block');
+      div.css('overflow', 'hidden');
+      div.css('cursor', 'pointer');
+      div.append(img).append('<div>' + title_text + '</div>');
+
+      $(videos).append(div).append(' ');
+    });
+  });
+});
